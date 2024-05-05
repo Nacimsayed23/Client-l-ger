f548874427ecc0f5268a0a36e3829a85fb3d61f2
Nacimsayed23 <163854754+Nacimsayed23@users.noreply.github.com>
2024-05-05 20:16:59 +0200

    modif

--- SiteLeboncoin1/SiteLeboncoin/lbc.sql
+++ SiteLeboncoin1/SiteLeboncoin/lbc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 08 mars 2024 à 00:45
+-- Généré le : dim. 05 mai 2024 à 20:15
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -43,6 +43,15 @@ CREATE TABLE `annonce` (
   `time` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Déchargement des données de la table `annonce`
+--
+
+INSERT INTO `annonce` (`ida`, `titre`, `vendeur`, `date`, `detail`, `photo`, `categorie`, `prix`, `etat`, `favoris`, `livraison`, `vue`, `time`) VALUES
+(8, 'Robe Élégante Zara : État Impeccable, Style Incont', 4, '2024-04-29 02:40:31', 'Plongez dans l\'élégance intemporelle avec cette magnifique robe de la marque Zara, en parfait état. Conçue avec soin et attention aux détails, cette pièce allie confort et style pour vous offrir une silhouette impeccable à chaque occasion. Le tissu de qualité assure un ajustement parfait, tandis que le design sophistiqué ajoute une touche de glamour à votre garde-robe. Ne manquez pas cette opportunité de vous démarquer avec cette robe Zara qui incarne le raffinement et la classe. Commandez dès maintenant et faites sensation à chaque sortie!', 'image/annonce/8.png', 1, 85, 'Très bon état', 1, 0, 0, 1714351231),
+(9, 'Ensemble Homme : Élégance et Qualité Assurées!', 4, '2024-04-29 03:05:02', 'Ensemble pour homme en excellent état, alliant style et confort. Parfait pour toutes occasions. Disponible dès maintenant.', 'image/annonce/Ensemble Homme : Élégance et Qualité Assurées!.png', 2, 30, 'Bon état', 0, 0, 0, 1714352702),
+(10, 'Ensemble Femme : État Satisfaisant, Style Irrésist', 4, '2024-04-29 03:13:56', 'Découvrez cet ensemble pour femme, en état satisfaisant, qui offre un style irrésistible à petit prix! Composé d\'une veste et d\'une jupe assortie, cet ensemble vous permettra de créer des looks tendance et polyvalents pour diverses occasions. Bien que présentant quelques signes d\'usage, cet ensemble conserve tout son charme et sa qualité.', 'image/annonce/Ensemble Femme : État Satisfaisant, Style Irrésistible!.png', 1, 30, 'Satisfaisant', 0, 0, 0, 1714353236);
+
 -- --------------------------------------------------------
 
 --
@@ -59,11 +68,11 @@ CREATE TABLE `categorie` (
 --
 
 INSERT INTO `categorie` (`idc`, `nomCat`) VALUES
-(1, 'mobilier salon'),
-(2, 'mobilier de salle manger'),
-(3, 'mobilier chambre'),
-(4, 'mobilier salle de bain'),
-(5, 'mobilier cuisine');
+(1, 'Femme'),
+(2, 'Homme'),
+(3, 'Enfant'),
+(4, 'Chaussure '),
+(5, 'Sacs à main');
 
 -- --------------------------------------------------------
 
@@ -90,6 +99,13 @@ CREATE TABLE `favoris` (
   `ida` int(2) NOT NULL,
   `idu` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `favoris`
+--
+
+INSERT INTO `favoris` (`idf`, `ida`, `idu`) VALUES
+(11, 8, 4);
 
 -- --------------------------------------------------------
 
@@ -126,6 +142,13 @@ CREATE TABLE `user` (
   `avatar` varchar(255) DEFAULT NULL,
   `statue` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`idu`, `nom`, `prenom`, `mail`, `tel`, `mdp`, `numRue`, `nomRue`, `nomVille`, `cp`, `avatar`, `statue`) VALUES
+(4, 'marwa', 'nacim', 'marwa.nacimsayed@gmail.com', '98766544', '18338fde69bfd465536bb6483d3cd256', '4556788', '56XGX', 'DALL', '8765', NULL, 0);
 
 --
 -- Index pour les tables déchargées
@@ -175,7 +198,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `annonce`
 --
 ALTER TABLE `annonce`
-  MODIFY `ida` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ida` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `categorie`
@@ -193,7 +216,7 @@ ALTER TABLE `conversation`
 -- AUTO_INCREMENT pour la table `favoris`
 --
 ALTER TABLE `favoris`
-  MODIFY `idf` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idf` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `message`
@@ -205,7 +228,7 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `idu` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idu` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
